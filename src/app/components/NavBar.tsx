4679a024f7edf8590cc8cee4edee1580c706dcfa
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2023-05-05 14:37:50 -0300

    Responsivity complete 3

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     <nav className="fixed mx-auto h-[76px] w-screen content-center bg-nav-bar-nude">
       <div className="mr-1 flex w-fit flex-row pt-2 align-middle tablet:mx-auto lg:mx-auto">
         <div
-          className={`${montserrat.className} pl-20 pt-5 text-[0.76rem] tablet:mx-auto tablet:pt-4 tablet:text-xl lg:mx-auto lg:pr-[5px] lg:pt-[8px] lg:text-4xl`}
+          className={`${montserrat.className} pl-20 pt-5 text-[0.68rem] tablet:mx-auto tablet:pt-4 tablet:text-xl lg:mx-auto lg:pr-[5px] lg:pt-[8px] lg:text-4xl`}
         >
           DAI OLIVEIRA - SOBRANCELHAS E CÍLIOS
         </div>
